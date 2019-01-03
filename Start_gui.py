f12cdc1ee7f2400594b569cc88d2d403afc82cfd
JulianSobott <julian.sobott@gmail.com>
2019-01-03 14:50:40 +0100

    Add MainWindow + ControlWindow

--- Start_gui.py
+++ Start_gui.py
@@ -4,7 +4,7 @@ from os import getcwd
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5 import uic
-from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtCore import QRect
 
 
 class Window(QtWidgets.QMainWindow):
@@ -15,8 +15,38 @@ class Window(QtWidgets.QMainWindow):
         self.show()
 
 
+class MainWindow(QtWidgets.QMainWindow):
+    TITLE = "Mower simulation"
+    SIZE = QRect(0, 0, 500, 600)
+
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle(self.TITLE)
+        self.setGeometry(self.SIZE)
+        self.show()
+
+
+class ControlWindow(QtWidgets.QMainWindow):
+    TITLE = "Mower simulation CONTROL"
+    SIZE = QRect(600, 100, 500, 600)
+
+    def __init__(self):
+        super().__init__(parent=None)
+
+        self.setWindowTitle(self.TITLE)
+        self.setGeometry(self.SIZE)
+
+        cb = QtWidgets.QCheckBox('Show title', self)
+        cb.move(20, 20)
+        cb.toggle()
+        #cb.stateChanged.connect(self.changeTitle)
+
+        self.show()
+
+
 if __name__=='__main__':
     app=QtWidgets.QApplication(sys.argv)
-    w = Window("Window1")
-    w2 = Window("Window 2")
+    #ex = Example()
+    mainWindow = MainWindow()
+    controlWindow = ControlWindow()
     sys.exit(app.exec())
